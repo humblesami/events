55d4d206b5327bfbf4269230b458b17079a6dbf4
asfandyar24 <asfand.yar@digitalnet.com>
2018-11-14 16:16:41 +0500

    button shown

--- dn_base/static/js/dn_login.js
+++ dn_base/static/js/dn_login.js
@@ -1,8 +1,8 @@
 var dn_footer = $('.dn_footer');
 dn_footer.html('Copyright © DigitalNet Technology Solutions. All Rights Reserved.');
 $('.oe_login_form').prepend('<img src="/dn_base/static/img/logo.png"/>');
-document.getElementById("button1").style.display = "none";
-console.log('11111111')
+//document.getElementById("button1").style.display = "none";
+//console.log('11111111')
 try
 {
     dn_json_rpc('/website/recaptcha/',{},function(result){
