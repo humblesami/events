7062de3b9118194f6f9807e569e9f205a922da20
asfandyar24 <asfand.yar@digitalnet.com>
2018-11-22 20:43:44 +0500

    captcha remvoal

--- website_base_recaptcha/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-
-from . import res_config
-from . import  website_base_recaptcha
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
