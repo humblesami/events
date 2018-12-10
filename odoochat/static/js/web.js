a05045032e444729cb9843311f6abecc0b2bad41
asfandyardigital <asfand.yar@digitalnet.com>
2018-12-10 19:50:59 +0500

    hide debug button

--- odoochat/static/js/web.js
+++ odoochat/static/js/web.js
@@ -4,6 +4,16 @@
 try{
     if($ && !odoo.odoochat){
         odoo.odoochat = 1;
+        if(odoo.session_info.uid!=1){
+        if (window.location.search =="?debug" || window.location.search =="?debug=1" ){
+        [1000, 3000, 5000].forEach(function(time){
+    setTimeout(function(){
+    $('.o_debug_manager').hide()
+    }, time);
+    });
+
+            }
+        }
         $('body').append('<link rel="stylesheet" href="/odoochat/static/css/chat.css"/>');
         document.writeln('<script src="/odoochat/static/js/socket.io.js"></script>');
         document.writeln('<script src="/odoochat/static/js/chat.js"></script>');
