ef8aa469d92dc10e4aac6d2c2f31530274139417
samiciit <sami.akram@digitalnet.com>
2019-02-07 19:55:04 +0500

    rtc related methods encapsulated in their models now

--- odoochat/controllers/controllers.py
+++ odoochat/controllers/controllers.py
@@ -16,72 +16,6 @@ class Oddochat(http.Controller):
         res = getActiveUserMessage(kw)
         return res
 
-    @http.route('/set-message-status', type="http", csrf=False, auth='public', cors='*')
-    def setMessageStatus(self, **kw):
-        try:
-            uid = ws_methods.check_auth(kw)
-            if not uid:
-                return ws_methods.not_logged_in()
-            kw['uid'] = uid
-            res = set_message_status(kw)
-            return ws_methods.http_response('', res['data'])
-        except:
-            return ws_methods.handle()
-
-    @http.route('/save-message', type="json", csrf=False, auth='public', cors='*')
-    def set(self):
-        try:
-            kw = request.jsonrequest
-            auth = kw.get('auth')
-            if not auth:
-                auth = kw
-            uid = ws_methods.check_auth(auth)
-            if not uid:
-                return ws_methods.not_logged_in()
-            values = kw.get('req_data')
-            if not values:
-                values = kw
-            res = save_messages(values)
-            return res['data']
-        except:
-            return ws_methods.handle()
-
-def save_messages(values):
-    try:
-        req_env = http.request.env
-        sender = values.get('sender')
-        to = values.get('to')
-        content = values.get('content')
-        message = req_env['odoochat.messages'].sudo().create({'sender': sender, 'to': to, 'content': content})
-        props = ['content', 'id', 'create_date', 'read_status', 'sender', 'to']
-        message = ws_methods.object_to_json_object(message, props)
-        res = ws_methods.http_response('', message)
-        res = {
-            'events': [
-                {'data': res, 'name': 'message', 'audience': [message['to']]}
-            ],
-            'data': res
-        }
-        return res
-    except:
-        return ws_methods.handle()
-
-def set_message_status(values):
-    try:
-        req_env = http.request.env
-        message_id = values.get('message_id')
-        filters = [('id', '=', message_id)]
-        req_env['odoochat.messages'].sudo().search(filters).write({'read_status': True})
-        res = ws_methods.http_response('', 'ok')
-        res = {
-            'events': [
-                {'data': res, 'name': 'set_message_status', 'audience': [values['uid']]}
-            ],
-            'data': res
-        }
-        return res
-    except:
-        return ws_methods.handle()
 
 def getActiveUserMessage(kw):
     try:
@@ -96,14 +30,14 @@ def getActiveUserMessage(kw):
         to = kw.get('target_id')
 
         filters = [('sender','=',to), ('to','=',sender)]
-        req_env['odoochat.messages'].sudo().search(filters).write({'read_status':True})
+        req_env['odoochat.message'].sudo().search(filters).write({'read_status':True})
 
         db_filters = [('sender', 'in', [sender, to]), ('to', 'in', [to, sender])]
-        count = req_env['odoochat.messages'].search_count(db_filters)
+        count = req_env['odoochat.message'].search_count(db_filters)
         offset = count - 20
         if offset < 1:
             offset = 1
-        messages = req_env['odoochat.messages'].search(db_filters, offset=offset)
+        messages = req_env['odoochat.message'].search(db_filters, offset=offset)
         props = ['sender', 'to', 'content', 'create_date']
         messages_obj = ws_methods.objects_list_to_json_list(messages, props)
         res = ws_methods.http_response('', messages_obj)
