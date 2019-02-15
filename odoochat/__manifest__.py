ff98af2e0df419a6d42d1364de4244e76eebd0ae
humblesami <samiakram@live.com>
2019-02-15 14:11:46 +0500

    removed js from odoochat

--- odoochat/__manifest__.py
+++ odoochat/__manifest__.py
@@ -3,20 +3,19 @@
     'name': "odoochat",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Chat and Notifications""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "DigitalNet",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Notifications',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -26,7 +25,6 @@
     'data': [
         'security/ir.model.access.csv',
         'security/rules.xml',
-        'views/views.xml',
         'views/templates.xml',
         'views/notification.xml',
     ],
