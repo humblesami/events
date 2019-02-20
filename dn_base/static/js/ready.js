00dbc48eea60633a6109ace4c90ae446d01c780a
humblesami <samiakram@live.com>
2019-02-21 00:56:52 +0500

    starting angular1

--- dn_base/static/js/ready.js
+++ dn_base/static/js/ready.js
@@ -58,12 +58,12 @@ function load_dn_assets()
         document.writeln('<script src="/dn_base/static/js/doc_preview.js?v='+dnow+'"></script>');
         document.writeln('<script src="/dn_base/static/js/libs/jquery.ui.touch.min.js"></script>');
 
-                	document.writeln(`
-
-<script type="text/javascript" src="/meeting_point/static/meetvue/polyfills.js"></script>
-<script type="text/javascript" src="/meeting_point/static/meetvue/vendor.js"></script>
-<script type="text/javascript" src="/meeting_point/static/meetvue/main.js"></script>
-`);
+//                	document.writeln(`
+//
+//<script type="text/javascript" src="/meeting_point/static/meetvue/polyfills.js"></script>
+//<script type="text/javascript" src="/meeting_point/static/meetvue/vendor.js"></script>
+//<script type="text/javascript" src="/meeting_point/static/meetvue/main.js"></script>
+//`);
 
         $(function(){
             doc_preview.init();
