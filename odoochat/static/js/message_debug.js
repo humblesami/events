b6242064fe8d94dc1740d8006f3a4a718df3000f
samiciit <sami.akram@digitalnet.com>
2019-03-01 17:20:17 +0500

    messenger top bar

--- odoochat/static/js/message_debug.js
+++ odoochat/static/js/message_debug.js
@@ -1,6 +1,9 @@
 (function(){
     var dnow = Date();    
     var libs = '<link rel="stylesheet" href="/odoochat/static/css/chat.css?v='+dnow+'"/>';
-    libs += '<script type="text/javascript" src="/meeting_point/static/meetvue/runtime.js?v='+dnow+'"></script>';
     $('#messenger_libs').append(libs);
+    setTimeout(function(){
+        libs = '<script type="text/javascript" src="/meeting_point/static/meetvue/runtime.js?v='+dnow+'"></script>';
+        $('#messenger_libs').append(libs);
+    },50);    
 })()
