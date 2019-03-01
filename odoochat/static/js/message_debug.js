9b0d3d8cd7eed7e66161a755e7ed4108fe66d2ee
samiciit <sami.akram@digitalnet.com>
2019-03-01 16:36:24 +0500

    messenger container

--- odoochat/static/js/message_debug.js
+++ odoochat/static/js/message_debug.js
@@ -1,9 +1,6 @@
 (function(){
-    var dnow = Date();
-    console.log(12, dnow);
-    var libs = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css?v='+dnow+'"/>';
-    libs += '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css?v='+dnow+'"/>';
-    libs += '<link rel="stylesheet" href="/meeting_point/static/css/chat.css?v='+dnow+'"/>';
+    var dnow = Date();    
+    var libs = '<link rel="stylesheet" href="/odoochat/static/css/chat.css?v='+dnow+'"/>';
     libs += '<script type="text/javascript" src="/meeting_point/static/meetvue/runtime.js?v='+dnow+'"></script>';
     $('#messenger_libs').append(libs);
 })()
