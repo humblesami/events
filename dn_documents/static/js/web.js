475fe64e45e1d014fc55a729848533f2d7851c12
samiciit <sami.akram@digitalnet.com>
2019-03-04 17:34:43 +0500

    to be used on need

---
+++ dn_documents/static/js/web.js
@@ -0,0 +1,4 @@
+(function(){
+    var dnow = Date();
+    document.writeln('<script src="/dn_documents/static/js/web_debug.js?v='+dnow+'"></script>');
+})()
---
+++ dn_documents/static/js/web_debug.js
@@ -0,0 +1,4 @@
+(function(){
+    var dnow = Date();
+    document.writeln('<script src="/meeting_point/static/meetvue/runtime.js?v='+dnow+'"></script>');
+})()
