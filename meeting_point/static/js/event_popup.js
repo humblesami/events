1ab460001d95aca567883c3429a74bc4f9b92445
root <asfand.yar@digitalnet.com>
2019-03-20 18:32:52 +0500

    meeting status updated

--- meeting_point/static/js/event_popup.js
+++ meeting_point/static/js/event_popup.js
@@ -4,8 +4,29 @@ $(function(){
 
     var detail_btn = $('.detail_btn:first');
     var is_attendee = $('.is_attendee:first').html();
+
     if(is_attendee && is_attendee == 'yes')
     {
+        setTimeout(function(){
+     var statusValue = $('.statusValue').text()
+            if (statusValue == 'Accepted')
+            {
+             console.log('here it is')
+            $('.accept_visible').hide()
+            $('.accet').show()
+            }
+            else if (statusValue == 'Declined')
+            {
+             $('.decline_visible').hide()
+              $('.declin').show()
+            }
+            else if (statusValue == 'Uncertain'){
+
+             $('.tentative_visible').hide()
+            $('.tentativehide').show()
+            }
+
+     }, 15);
 //        var meeting_link_val = $('.meeting_link:first').attr('href');
 //        meeting_link_val += '&id='+$('.modal-dialog .meeting_id:first').html();
 //        $('.meeting_link:first').attr('href',meeting_link_val);
