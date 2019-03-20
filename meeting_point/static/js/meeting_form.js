f226cbb09134470652316da0a8cdfb204c535144
root <asfand.yar@digitalnet.com>
2019-03-20 17:25:44 +0500

    button status

--- meeting_point/static/js/meeting_form.js
+++ meeting_point/static/js/meeting_form.js
@@ -8,5 +8,24 @@ $(function(){
         var padding = parseFloat(form_sheet.css('padding-left')) + parseFloat(form_sheet.css('padding-left'));
         var width = form_sheet.width() + padding;
         $('.comments.oe_read_only').width(width);
+
+        var statusValue = $('.statusValue').text()
+            if (statusValue == 'Accepted')
+            {
+            $('.accept_visible').hide()
+            $('.accet').show()
+            }
+            else if (statusValue == 'Declined')
+            {
+             $('.decline_visible').hide()
+              $('.declin').show()
+            }
+            else if (statusValue == 'Uncertain'){
+
+             $('.tentative_visible').hide()
+            $('.tentativehide').show()
+            }
     }, 15);
+
+
 });
