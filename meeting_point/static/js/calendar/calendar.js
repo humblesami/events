43c6049280c11c1c4bd9b11c4af03d1f33097729
root <asfand.yar@digitalnet.com>
2019-03-22 14:08:32 +0500

    time out in calendar.js

--- meeting_point/static/js/calendar/calendar.js
+++ meeting_point/static/js/calendar/calendar.js
@@ -1,105 +1,104 @@
-$('#loaderContainerajax').show();
-setTimeout(function(){
+
 $(function(){
-    var str = $('span[name="public_event"]').html();
-    var events = JSON.parse(str);
-    $('#calendar').fullCalendar({
-
-        header: {
-            left: 'year,month,agendaWeek,agendaDay',
-            center: 'title',
-            right: 'prev,next, today'
-        },
-          views: {
-    agendaFourDay: {
-      type: 'agenda',
-      duration: { days: 4 },
-      buttonText: 'Schedule'
-    }
-  },
-        //defaultDate: '2018-03-12',
-        navLinks: true, // can click day/week names to navigate views
-        yearColumns: 3,
-        editable: true,
-        eventLimit: true, // allow "more" link when too many events
-        events: events
-    });
-
-    if ($('.fc-schedule-button').length == 0) {
-
-
-
-        function hour_minutes(dt) {
-            if (typeof (dt) == "string")
-                dt = new Date(dt);
-            var hour = dt.getHours()+5;
-            var minut = dt.getMinutes();
-            if (minut < 10) {
-                minut = '0' + minut;
-            }
-            return hour + ':' + minut;
-}
-
 
         var str = $('span[name="public_event"]').html();
         var events = JSON.parse(str);
+        $('#calendar').fullCalendar({
 
-
-
-        var schedule = '<div class="container-fluid schedule-container schedule-wrap">';
-        for (var i = 0; i < events.length; i++) {
-                if (events[i].attendee) {
-                   var parts = events[i].datestart .split('-')
-                   var mydate = new Date(parts[0], parts[1] - 1, parts[2]);
-                   parts = mydate.toDateString()
-                   parts = parts.split(' ')
-                   dateValue = parts[1] + ' '+parts[2]+ ',' + parts[3]
-                schedule += '<div event_id='+events[i].id+' class="scheduleDetailOpener row fc-event">';
-                schedule += '<div class="col"> <span>' + dateValue + '</span></div>';
-                schedule += '<div class="col">'+ hour_minutes(new Date(events[i].startSchedule)) + ' - ' + hour_minutes(new Date(events[i].endSchedule)) + '</div>';
-                schedule += '<div class="col fc-title">' + events[i].title + '</div>';
-                schedule += '</div>'
-            }
+            header: {
+                left: 'year,month,agendaWeek,agendaDay',
+                center: 'title',
+                right: 'prev,next, today'
+            },
+              views: {
+        agendaFourDay: {
+          type: 'agenda',
+          duration: { days: 4 },
+          buttonText: 'Schedule'
         }
-        schedule += '</div>';
-//        schedule = $(schedule);
-//        schedule.find('.scheduleDetailOpener').click(scheduleDetails);
-        var btn = $('<button type="button" class="fc-schedule-button fc-button fc-state-default fc-corner-right">Schedule</button>')
-        $('.fc-button-group:first').append(btn);
-
-        btn.click(function showSchedule() {
-            $('.schedule-container').show();
-            $('.fc-view-container').empty().html(schedule);
-            $('.fc-prev-button').hide();
-            $('.fc-next-button').hide();
-            $('.fc-center').hide();
-            $('.fc-today-button').hide();
-            $('.fc-state-active').removeClass('fc-state-active');
-            btn.addClass('fc-state-active');
+      },
+            //defaultDate: '2018-03-12',
+            navLinks: true, // can click day/week names to navigate views
+            yearColumns: 3,
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            events: events
         });
+
+        if ($('.fc-schedule-button').length == 0) {
+
+
+
+            function hour_minutes(dt) {
+                if (typeof (dt) == "string")
+                    dt = new Date(dt);
+                var hour = dt.getHours()+5;
+                var minut = dt.getMinutes();
+                if (minut < 10) {
+                    minut = '0' + minut;
+                }
+                return hour + ':' + minut;
     }
 
 
-    if(!odoo['meeting_point_calendar_js'])
-    {
+            var str = $('span[name="public_event"]').html();
+            var events = JSON.parse(str);
 
-        odoo['meeting_point_calendar_js'] = 1;
-        $(document).on("click",".fc-event", function(e){
-            var id = $(this).find('.fc-title').text();
-            data = 0
-            for(val in events){
-            if(events[val].title === id ){
-                data = events[val].id
-               }
+
+
+            var schedule = '<div class="container-fluid schedule-container schedule-wrap">';
+            for (var i = 0; i < events.length; i++) {
+                    if (events[i].attendee) {
+                       var parts = events[i].datestart .split('-')
+                       var mydate = new Date(parts[0], parts[1] - 1, parts[2]);
+                       parts = mydate.toDateString()
+                       parts = parts.split(' ')
+                       dateValue = parts[1] + ' '+parts[2]+ ',' + parts[3]
+                    schedule += '<div event_id='+events[i].id+' class="scheduleDetailOpener row fc-event">';
+                    schedule += '<div class="col"> <span>' + dateValue + '</span></div>';
+                    schedule += '<div class="col">'+ hour_minutes(new Date(events[i].startSchedule)) + ' - ' + hour_minutes(new Date(events[i].endSchedule)) + '</div>';
+                    schedule += '<div class="col fc-title">' + events[i].title + '</div>';
+                    schedule += '</div>'
+                }
             }
-            $('.pblic_evnt_id:contains('+data+')').click();
+            schedule += '</div>';
+    //        schedule = $(schedule);
+    //        schedule.find('.scheduleDetailOpener').click(scheduleDetails);
+            var btn = $('<button type="button" class="fc-schedule-button fc-button fc-state-default fc-corner-right">Schedule</button>')
+            $('.fc-button-group:first').append(btn);
+
+            btn.click(function showSchedule() {
+                $('.schedule-container').show();
+                $('.fc-view-container').empty().html(schedule);
+                $('.fc-prev-button').hide();
+                $('.fc-next-button').hide();
+                $('.fc-center').hide();
+                $('.fc-today-button').hide();
+                $('.fc-state-active').removeClass('fc-state-active');
+                btn.addClass('fc-state-active');
+            });
+        }
 
 
-        });
-    }
+        if(!odoo['meeting_point_calendar_js'])
+        {
+
+            odoo['meeting_point_calendar_js'] = 1;
+            $(document).on("click",".fc-event", function(e){
+                var id = $(this).find('.fc-title').text();
+                data = 0
+                for(val in events){
+                if(events[val].title === id ){
+                    data = events[val].id
+                   }
+                }
+                $('.pblic_evnt_id:contains('+data+')').click();
+
+
+            });
+        }
+
 });
-$('#loaderContainerajax').hide();
-},100)
 
 //   customButtons: {
 //        add_event: {
