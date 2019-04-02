3ed96d17de58678a3e74938e3d2e83be36a47404
faizan <faizan.tahir@digitalnet.com>
2019-04-02 19:08:22 +0500

    Revert "pdftotext instead of pdfminer"
    This reverts commit 184100c7

--- dn_documents/models/allfiles.py
+++ dn_documents/models/allfiles.py
@@ -12,6 +12,10 @@ import pdf2image as pdf2image
 from pytesseract import pytesseract
 
 from odoo import models, fields, api
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 
 from odoo.addons.dn_base.statics import scan_virus,raise_dn_model_error
 
@@ -176,10 +180,24 @@ class AllFiles(models.Model):
                      pth + filename])
                 res = open(pth + '_new.pdf', 'rb')
             content=""
-            pdf = pdftotext.PDF(res)
-            for p in pdf:
-                content+=p
+            parser = PDFParser(res)
+            doc = PDFDocument()
+            parser.set_document(doc)
+            doc.set_parser(parser)
+            doc.initialize('')
+            rsrcmgr = PDFResourceManager()
+            laparams = LAParams()
+            laparams.char_margin = 1.0
+            laparams.word_margin = 1.0
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
 
+            for page in doc.get_pages():
+                interpreter.process_page(page)
+                layout = device.get_result()
+                for lt_obj in layout:
+                    if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                        content += lt_obj.get_text()
 
             # print(content)
             if ext != "pdf":
