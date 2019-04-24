4f049bec320a2da5d537eb20a911efa162e1ce54
samiciit <sami.akram@digitalnet.com>
2019-04-24 21:01:24 +0500

    allowed port

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd9phtmv5b^cqr$daog097))s@$776gvk$-ca&shxt&re*r31bn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.16.21.170']
+ALLOWED_HOSTS = ['172.16.21.170', '34.74.192.194']
 
 
 EMAIL_HOST = 'smtp.gmail.com'
