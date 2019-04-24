52869721e8084cbab3ebd49c6d7d491e16ab9f0c
samiciit <sami.akram@digitalnet.com>
2019-04-24 17:30:31 +0500

    got chat friends

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from authsignup.models import AuthUser
 
 # Create your models here.
 class Message(models.Model):
@@ -11,3 +12,13 @@ class Message(models.Model):
     @classmethod
     def get(cls, request, params):
         return {'error': 'Not implemented'}
+
+
+class AuthUserChat(AuthUser):
+    class Meta:
+        proxy = True
+    @classmethod
+    def get_data(cls, request, params):
+        data = User.objects.exclude(id=params['id']).values('id', 'username')
+        data = {'friends' : list(data), 'notifications': [] }
+        return data
