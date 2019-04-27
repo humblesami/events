ca29ac3ba76f2d9a32a5295c7f0b999a75733c57
humblesami <samiakram@live.com>
2019-04-28 01:02:09 +0500

    public rest

--- mainapp/rest_api.py
+++ mainapp/rest_api.py
@@ -7,6 +7,42 @@ import traceback
 from django.apps import apps
 from django.http import HttpResponse
 
+public_methods = {
+    'authsignup':{
+        'AuthUser':{
+            'login_user': 1
+        }
+    }
+}
+
+def public(request):
+    try:
+        kw = request.POST
+        res = 'Unknown'
+        if not kw:
+            kw = request.GET
+        kw = json.loads(kw['input_data'])
+        args = kw['args']
+        try:
+            res = args['app'][args['model'][args['method']]]
+        except:
+            res = 'Invalid method call'
+            return produce_result(res, args)
+        params = kw['params']
+        model = apps.get_model(args['app'], args['model'])
+        method_to_call = getattr(model, args['method'])
+        res = method_to_call(request, params)
+        return produce_result(res, args)
+    except:
+        eg = traceback.format_exception(*sys.exc_info())
+        errorMessage = ''
+        for er in eg:
+            errorMessage += " " + er
+        res = {'error' : errorMessage}
+        res = json.dumps(res)
+        return HttpResponse(res)
+
+
 @csrf_exempt
 @api_view(["GET", "POST"])
 def secure(request):
@@ -35,27 +71,6 @@ def secure(request):
         res = json.dumps(res)
         return HttpResponse(res)
 
-
-def public(request):
-    try:
-        kw = request.POST
-        if not kw:
-            kw = request.GET
-        kw = json.loads(kw['input_data'])
-        args = kw['args']
-        params = kw['params']
-        model = apps.get_model(args['app'], args['model'])
-        method_to_call = getattr(model, args['method'])
-        res = method_to_call(request, params)
-        return produce_result(res, args)
-    except:
-        eg = traceback.format_exception(*sys.exc_info())
-        errorMessage = ''
-        for er in eg:
-            errorMessage += " " + er
-        res = {'error' : errorMessage}
-        res = json.dumps(res)
-        return HttpResponse(res)
 
 def produce_result(res, args):
     if res:
