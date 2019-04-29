98598d923dba4afd14b766db06a040633b4c8b2a
samiciit <sami.akram@digitalnet.com>
2019-04-29 17:49:28 +0500

    user in user data on verify

--- chat/models.py
+++ chat/models.py
@@ -18,29 +18,35 @@ class AuthUserChat(models.Model):
     @classmethod
     def verify_chat_user(cls, request, params):
         uid = params['id']
-        mp_users = Profile.objects.exclude(id=uid)
+        mp_users = Profile.objects.filter()
         unseenMessages = 0
         friendList = {}
         friendIds = []
         http_host = request.META.get('HTTP_HOST')
+        req_user = {}
         if not http_host:
             http_host = ''
         for friendObj in mp_users:
-            friend = {
-                'id': friendObj.id,
-                'name': friendObj.fullname(),
-                'photo': http_host+friendObj.image.url
-            }
-            # if friendObj.has_group('meeting_point.group_meeting_staff') or friendObj.has_group(
-            #         'meeting_point.group_meeting_admin'):
-            #     friend['type'] = 'staff'
-            # else:
-            #     friend['type'] = 'director'
-
-            db_filters = ()
-            friend['unseen'] = len(Message.objects.filter(sender=friendObj.id, read_status=False, to=uid))
-            unseenMessages += friend['unseen']
-            friendList[friend['id']] = friend
-            friendIds.append(friend['id'])
-        data = {'friends' : friendList, 'notifications': [], 'unseen': unseenMessages }
+            if friendObj.id != uid:
+                friend = {
+                    'id': friendObj.id,
+                    'name': friendObj.fullname(),
+                    'photo': http_host+friendObj.image.url
+                }
+                # if friendObj.has_group('meeting_point.group_meeting_staff') or friendObj.has_group(
+                #         'meeting_point.group_meeting_admin'):
+                #     friend['type'] = 'staff'
+                # else:
+                #     friend['type'] = 'director'
+                friend['unseen'] = len(Message.objects.filter(sender=friendObj.id, read_status=False, to=uid))
+                unseenMessages += friend['unseen']
+                friendList[friend['id']] = friend
+                friendIds.append(friend['id'])
+            else:
+                req_user =  {
+                    'id': friendObj.id,
+                    'name': friendObj.fullname(),
+                    'photo': http_host+friendObj.image.url
+                }
+        data = {'friends' : friendList, 'notifications': [], 'unseen': unseenMessages, 'user': req_user }
         return data
