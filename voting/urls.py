d4e3d3b82f5514471a892d370422ac6025456ded
faizanansaridn <muhammad.faizan@digitalnet.com>
2019-05-03 20:04:41 +0500

    record signature from url is removed

--- voting/urls.py
+++ voting/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:voting_id>', views.detail, name = 'detail'),
     path('<int:voting_id>/answer', views.answer, name = 'answer'),
-    path('signature/<str:res_model>/<int:res_id>', views.record_signature, name = 'signature'),        
     path('<int:meeting_id>/topic', views.topic, name='topic')
 ]
