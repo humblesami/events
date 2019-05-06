23f0810eb2a537c8e8b0e06abadd34e7511ffb40
faizan <faizan.tahir@digitalnet.com>
2019-05-06 18:58:49 +0500

    view.py for secure media

---
+++ mainapp/views.py
@@ -0,0 +1,19 @@
+import os
+
+from django.contrib.auth.decorators import login_required
+from django.http.response import FileResponse, HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from documents.file import File
+from mainapp.settings import MEDIA_ROOT
+
+
+@login_required()
+def serve_protected_document(request,folder, file):
+    # document = get_object_or_404(File, attachment="converted/" + file)
+
+    path = MEDIA_ROOT + '/' + folder + '/' +file
+    response = FileResponse(open(path,'rb'))
+    # response["Content-Disposition"] = "attachment; filename=" + file
+
+    return response
