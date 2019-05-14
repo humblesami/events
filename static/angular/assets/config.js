da8bc300c7b64cb647fa7fbf752902a6b472398d
humblesami <samiakram@live.com>
2019-05-14 20:12:38 +0500

    config changes

--- static/angular/assets/config.js
+++ static/angular/assets/config.js
@@ -26,28 +26,38 @@ var network_config = {
     app_name : 'MeetingPoint',
 	show_logs : ['socket','ajax_before','ajax_success']
 };
+var network_config_https = {
+	server_base_url:'https://172.16.21.170:8000',
+	server_db : 'demo1',
+	live : false,
+	site_url: '',
+    chat_server : 'https://172.16.21.170:3000',
+    app_name : 'MeetingPoint',
+	show_logs : ['socket','ajax_before','ajax_success']
+};
 
-if(window.location.toString().indexOf('localhost') > -1)
+var current_site_base_url = window.location.origin.toString();
+if(current_site_base_url.indexOf('localhost') > -1)
 {   
     site_config = site_config_local;
 }
 else
 {
-    if(window.location.toString().indexOf('172.16') > -1)
+	if(current_site_base_url.startsWith('https://172'))
+	{
+		site_config = network_config_https;
+	}
+    else if(current_site_base_url.indexOf('172.16') > -1)
     {
         site_config = network_config;
     }
 }
-
-if(window["odoo"]){
-    site_config.server_db = window["odoo"].session_info.db
-}
-
-site_config.site_url = window.location.origin;
-if(window.location.origin.toString().indexOf( window.location.origin+'/web') > -1)
+if(current_site_base_url.indexOf('wwww.') > -1)
 {
-    site_config.site_url = window.location.origin+'/web';
+	site_config.server_base_url = site_config.server_base_url.replace('https://','https://www.')
 }
+
+site_config.site_url = current_site_base_url;
 
 site_config.public_urls = ['login']
 //console.log(site_config);
