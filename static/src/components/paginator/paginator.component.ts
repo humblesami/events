ae0059d81c7f82da358b73fcc813855a090afba2
NomanHameed-DN <noman.hameed@digitalnet.com>
2019-08-09 19:38:02 +0500

    roster pagination

--- static/src/components/paginator/paginator.component.ts
+++ static/src/components/paginator/paginator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { HttpService } from '../../app/http.service';
+import { range } from 'rxjs';
 declare var $: any;
 
 @Component({
@@ -10,7 +11,11 @@ declare var $: any;
 export class PaginatorComponent implements OnInit {     
     @Input() count: number;   
     @Output() changedOffset: EventEmitter<number> =   new EventEmitter();
-    @Output() changedLimit: EventEmitter<number> = new EventEmitter()
+    @Output() changedLimit: EventEmitter<number> = new EventEmitter();
+    @Output() lastpage: EventEmitter<number>= new EventEmitter();
+    @Output() firstpage: EventEmitter<number>= new EventEmitter();
+    @Output() pagedata: EventEmitter<number>= new EventEmitter();
+    total_pages = []; 
     limit = 2;
     limit_options = [
         2,
@@ -29,43 +34,66 @@ export class PaginatorComponent implements OnInit {
         // console.log(Date(), new Date().getMilliseconds(), 113);
         this.httpService = httpServ;
         this.offset = 0;        
-        this.page_number = 1;        
+        this.page_number = 1;      
     }
 
     offset: number;
-
+    page_Data(event) {
+        this.offset = (event - 1) * this.limit;
+        this.page_number = event;
+        this.pagedata.emit(this.offset);
+    }
     change_page(change: number){
-        console.log(change, this.offset, this.limit, this.count);
-        if(change <= 1 && this.offset < 0)
-        {
-            this.offset = 0;
-            return;
+        var ppgn = this.page_number +=change;        
+        this.page_Data(ppgn);
+    }    
+    last_Page(change: number){
+        let lPage= Math.ceil(change/this.limit);
+        this.page_Data(lPage);      
+    }
+    first_Page(change: number){
+        this.page_Data(1);
+    }
+
+    shown_pages = [1,2,3,4,5];
+    all_pages(pages:number){
+        this.total_pages = [];
+        let lPage= Math.ceil(pages/this.limit);
+        for (let i = 1; i <= lPage; i++) {            
+            this.total_pages.push(i)
         }
-        else if(change >= 1 && this.offset + this.limit >= this.count)
+        if(this.shown_pages.length>this.total_pages.length)
         {
-            return;
+            this.shown_pages = this.total_pages;
         }
-        else{
-            let new_val = change * this.limit;        
-            this.offset = this.offset + new_val;
-            this.page_number += change;
-            this.changedOffset.emit(this.offset);
-            // console.log(this.offset, this.limit, this.count);
-        }        
     }
     change_limit(e){
-        this.limit = $(e.target).val();
+        console.log(this.offset,this.limit,  1411);
+        this.limit = Number($(e.target).val());
+        this.offset=0;
+        this.page_number=1;
+        this.all_pages(this.count);
         this.changedLimit.emit(this.limit);
-        // console.log(this.limit, this.offset, 1411);
+        console.log(this.offset,this.limit, 1411);
     }
+    
+    
+    
+
     public updateCount(total)
     {
         console.log(total, 14545);
     }
+
+    
     ngOnInit() {        
         // console.log(this.count, 199);
         let obj_this = this;
         window['wait_or_execute']
+        this.all_pages(this.count);
     }
 
+
+    
 }
+
