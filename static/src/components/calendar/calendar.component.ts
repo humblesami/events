7065c933b4b30783655c963867a36e2061fcb220
samiciit <sami.akram@digitalnet.com>
2019-08-22 12:45:40 +0500

    calendar popup

--- static/src/components/calendar/calendar.component.ts
+++ static/src/components/calendar/calendar.component.ts
@@ -13,14 +13,23 @@ declare var $: any;
 export class CalendarComponent implements OnInit {
     home_data: any;
     events = [];
+    selected_event: any;
     constructor(private httpService: HttpService,
         public router: Router,
         private sanitizer: DomSanitizer,
         private socketService: SocketService) {}
 
     ngOnInit() {
-        $('#event-summary tr').hide();
-        this.get_home_data()
+        let obj_this = this;
+        $('#event-summary .home-calendar-modal-detail>div').hide();
+        $('body').on('click', '.fc-event.grid-event', function(){            
+            var event_id = $(this).find('.fc-content:first').attr('event_id');
+            console.log(obj_this.events, 133, event_id);
+            obj_this.selected_event = obj_this.events.filter(function(item){
+                return item.id == event_id;
+            })
+        });
+        this.get_home_data();
     }
 
     get_home_data() {
@@ -88,13 +97,11 @@ export class CalendarComponent implements OnInit {
                     },
                     args: args
                 };
-                obj_this.httpService.get(input_data,
-                    (data: any) => {
-                        if (data)
-                            if (typeof(data) != "string") {
-                                obj_this.render_CalendarEvnetPopup(data);
-                            }
-                    }, null);
+                obj_this.httpService.get(input_data,function(data){
+                    console.log(data, 134);                    
+                    obj_this.selected_event = data;
+                    $('#calenderModal').modal('show');
+                }, null);
             },
             header: {
                 left: 'year,month,agendaWeek,agendaDay',
@@ -144,8 +151,6 @@ export class CalendarComponent implements OnInit {
     scheduleDetails(el) {
         let obj_this = this;
         var event_id = $(el).attr('event_id');
-        var req_url = '/meeting/summary'
-
         let args = {
             app: 'meetings',
             model: 'Event',
@@ -157,126 +162,17 @@ export class CalendarComponent implements OnInit {
             },
             args: args
         };
-        obj_this.httpService.get(input_data,
-            (data: any) => {
-                if (data)
-                    if (data)
-                        if (typeof(data) != "string") {
-                            obj_this.render_CalendarEvnetPopup(data);
-                        }
-            },
-            (error: any) => {})
+        obj_this.httpService.get(input_data,function(data){
+            console.log(data, 13888);
+            obj_this.selected_event = data;
+            $('#calenderModal').modal('show');
+        }, null)
     };
 
     render_CalendarEvnetPopup(result) {
         let obj_this = this;
-        var event_summary = $('#event-summary');
-        var calendar_modal = $('#calenderModal');
-        var upcoming_buttons = event_summary.find('.upcomingButton');
-        result.start = window['functions'].meeting_time_str(result.start)
-        result.stop = window['functions'].meeting_time_str(result.stop);
-        if (result.my_event) {
-            calendar_modal.find('.go_details').attr('id', result.id).show();
-            upcoming_buttons.show();
-            event_summary.find('.upcomingButton').show();
-            if (result.attendee_status) {
-                upcoming_buttons.find('.fa-check').remove();
-                upcoming_buttons.find('span[name="' + result.attendee_status + '"]').before('<i class="fa fa-check fa-lg" style="color:white" modifiers="{}"></i>');
-            }
-        } else {
-            calendar_modal.find('.go_details').attr('id', 0).hide();
-            upcoming_buttons.hide();
-        }
-
-        function calLocation(result) {
-            result['location'] = '';
-            result.location = result.location;
-        }
-        calLocation(result);
-        //console.log(result);
-        event_summary.find('tr').hide();
-
-        for (var key in result) {
-            if (!result[key])
-                continue;
-            var span = event_summary.find('span[name="' + key + '"]');
-            span.closest('tr').show();
-            if (key == 'pin' || key == 'conference_bridge_number' || key == 'video_call_link') {
-                if (result['conference_status'] != 'active') {
-                    span.closest('tr').hide();
-                    continue;
-                } else {
-                    if (key == 'video_call_link') {
-                        var video_call_link = '/conference/' + result['id'] + '/' + result['pin']
-                        var element = span.find('a');
-                        var base_url = window.location.origin.toString();
-                        //base_url = base_url.slice(0,-1);
-                        element.html(base_url + video_call_link);
-                        element.attr('href', base_url + video_call_link);
-                        span.closest('tr').show();
-                        continue;
-                    }
-                    span.closest('tr').show();
-                }
-            }
-            if (span.length > 0) {
-                span.html(result[key]);
-            }
-        }
-
-        //upcoming_buttons.attr('meetingid',result.id);
-        //calendar_modal.find('.modal-header:first h3');
-        calendar_modal.find('.modal-header:first').find('h3').html(result.name);
-        calendar_modal.find('.modal-body:first').html(event_summary.html());
-
-        upcoming_buttons = calendar_modal.find('.upcomingButton:first');
-
-        // calender event pop up buttons click listener
-        upcoming_buttons.children().click(function() {
-            var elbtn = $(this);
-            var response = elbtn.find('span').attr('name');
-            upcoming_buttons.find('i').remove();
-            elbtn.find('span').before('<i class="fa fa-check fa-lg" style="color:white" modifiers="{}"></i>');
-            window['is_popup'] = 1;
-            obj_this.respond_invitation(response, result.id)
-            $('#tdmrb' + result.id).find('i').remove();
-            $('#tdmrb' + result.id).find('span[name="' + response + '"]').before('<i class="fa fa-check fa-lg" style="color:white" modifiers="{}"></i>');
-        });
+        var calendar_modal = $('#calenderModal');        
         calendar_modal.modal('show');
-    }
-
-    respond_invitation(response: string, meet_id: string) {
-        let req_url = '/meeting/respond-invitation-json';
-        let obj_this = this;
-        let input_data = {
-            meeting_id: meet_id,
-            response: response,
-            no_loader: 1
-        };
-        
-        let pending_meetings = obj_this.home_data.to_do_items.pending_meetings;
-        var meeting_being_updated = pending_meetings.filter(function(item){
-            return item.id == meet_id;
-        });
-        if (response) {
-            let args = {
-                app: 'meetings',
-                model: 'Event',
-                method: 'respond_invitation'
-            }
-            let final_input_data = {
-                params: input_data,
-                args: args,
-                no_loader:1,
-            };
-            obj_this.httpService.get(final_input_data, function(data) {
-                if (!window['is_popup']) {
-                    meeting_being_updated.attendee_status = response;
-                } else {
-                    window['is_popup'] = 0;
-                }
-            }, null);
-        }
     }
 
     navigate_meeting() {
@@ -284,5 +180,4 @@ export class CalendarComponent implements OnInit {
         let id = document.getElementsByClassName('go_details')[0].id;
         obj_this.router.navigate(['/upcoming/meeting/' + id]);
     }
-
 }
