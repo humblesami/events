a16774bad4759cc7708eae4ff842026251c20398
humblesami <samiakram@live.com>
2019-08-24 22:29:35 +0500

    githelpercommit

---
+++ static/git_helper/js/git.js
@@ -0,0 +1,32 @@
+$('form:first').submit(function(e){
+    e.preventDefault();
+    var input_data = {};
+    var do_request = true;
+    $('.form-controls input').each(function (i, el) {
+        input_data[el.name] = el.value;
+    });
+    
+    
+    var url = '/pull';
+    if($('input[name="message"]').val())
+    {
+        url = '/push';
+    }
+    var options = {
+        url: url,
+        beforeSend:function(){
+            $('button').prop('disabled', true);
+        },
+        data:input_data,
+        success:function (data) {
+            console.log(data);
+        },
+        error:function (er) {
+            console.log(er);
+        },
+        complete:function(){
+            $('button').prop('disabled', false);
+        }
+    };
+    $.ajax(options);
+});
