c45d95776f52d6ad417d704e9791da29511d7cf2
NomanHameed-DN <noman.hameed@digitalnet.com>
2019-08-27 20:01:03 +0500

    onedrive

--- temp/urls.py
+++ temp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('drive', views.gdrive, name='gdrive'),
     path('drop', views.dropbox, name='gdrive'),
+    path('onedrive', views.onedrive, name='onedrive'),
     path('download', views.download, name='gdrive')
 ]
