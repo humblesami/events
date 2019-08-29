89cf0319a9f704496808e256ba342572de2daf47
samiciit <sami.akram@digitalnet.com>
2019-08-29 14:54:52 +0500

    home data

--- authsignup/models.py
+++ authsignup/models.py
@@ -60,6 +60,7 @@ class AuthUser(models.Model):
             if user and user.id:
                 profile = Profile.objects.filter(pk=user.id)
                 if (not profile) and user.is_superuser:
+                    login(request, user)
                     profile = Profile(user_ptr=user, email=user.email, password=password, username = user.username, is_superuser = user.is_superuser)
                     profile.save()
                 else:
