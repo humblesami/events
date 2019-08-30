f710695c2c121e0983ad7aef0cfd789362627ffd
samiciit <sami.akram@digitalnet.com>
2019-08-30 17:41:04 +0500

    messnger1

--- static/src/components/home/home.component.ts
+++ static/src/components/home/home.component.ts
@@ -66,7 +66,7 @@ export class HomeComponent implements OnInit {
             obj_this.home_data = home_data;
             home_data.video_ids = valid_videos;
             var to_do_items = home_data.to_do_items;
-            console.log(home_data);
+            // console.log(home_data);
             obj_this.to_do_count = to_do_items.pending_documents.length + to_do_items.pending_surveys.length + to_do_items.pending_votings.length;
         };
         let args = {
