be8ffc8d01b482db434fc76c561e9ec3fa7700a4
samiciit <sami.akram@digitalnet.com>
2019-09-03 17:40:13 +0500

    enno v kareen

---
+++ static/src/components/documents/documents.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
+
+@Component({
+    selector: 'app-documents',
+    templateUrl: './documents.component.html',
+    styleUrls: ['./documents.component.css']
+})
+export class DocumentsComponent implements OnInit {
+    httpService: HttpService;
+    @Input() parent_field: string;
+    @Input() parent_id: string;
+
+    @Input() res_app: string;
+    @Input() res_model: string;
+
+    docs = [];
+    constructor(private httpServ: HttpService) {
+        this.httpService = httpServ;  
+    }
+
+    ngOnInit() {
+        let obj_this = this;
+        let input_data = {
+            args:{
+                app:'documents',
+                model:'File',
+                method: 'get_attachments',
+            },
+            params:{
+                app: obj_this.res_app,
+                model: obj_this.res_model,
+                parent_field: obj_this.parent_field,
+                parent_id: obj_this.parent_id,
+            }
+        }
+        this.httpService.get(input_data, function(data){
+            for(var doc of data){
+                doc.routerLink = '/'+doc.type+'/doc/'+doc.id;
+            }
+            
+        }, null)
+    }
+
+}
