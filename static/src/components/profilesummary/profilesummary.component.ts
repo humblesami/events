a76833bbeced77bcac5254bd96f8ab51d3a59399
faridawan <farid.alvi@digitalnet.com>
2019-09-11 19:14:49 +0500

    Roster User Profiles Modal

--- static/src/components/profilesummary/profilesummary.component.ts
+++ static/src/components/profilesummary/profilesummary.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpService } from '../../app/http.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { SocketService } from 'src/app/socket.service';
-import { template } from '@angular/core/src/render3';
+declare var $:any;
 
 
 @Component({
@@ -33,9 +33,12 @@ export class ProfilesummaryComponent implements OnInit {
       args: args
     }
     obj_this.httpService.get(final_input_data, (data)=>{
-      console.log(data,1234);
+      // console.log(data,1234);
       obj_this.profile_data = data;
-      
+      setTimeout(function(){
+        // console.log($('.profile_summary_popup').closest('.modal-dialog').length,12321);
+        $('.profile_summary_popup').closest('.modal-dialog').addClass('modal-lg modal-dialog-centered');
+      },10);
     }, null)
   }
 
