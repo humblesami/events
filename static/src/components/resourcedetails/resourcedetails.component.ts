e85bbe7fbbe62ff387acc49de303045424132170
faridawan <farid.alvi@digitalnet.com>
2019-09-26 14:50:26 +0500

    Search in all components

--- static/src/components/resourcedetails/resourcedetails.component.ts
+++ static/src/components/resourcedetails/resourcedetails.component.ts
@@ -46,8 +46,7 @@ export class ResourceDetailsComponent implements OnInit {
         if(obj_this.advance_search)
         {
             $('.resources .search_options:first').show();
-            $('.bold-setting').addClass('text-primary').removeClass('text-muted').css("font-size", "1.2rem");
-            $('.bold-search').css("font-size", "1.3rem");
+            $('.bold-setting').addClass('text-primary').removeClass('text-muted small');
             if(is_not_root)
             {
                 obj_this.cookie_key = 'resources/inner/search';
@@ -67,8 +66,7 @@ export class ResourceDetailsComponent implements OnInit {
         }
         else{
             $('.resources .search_options:first').hide();
-            $('.bold-setting').addClass('text-muted').removeClass('text-primary').css("font-size", "1rem");
-            $('.bold-search').css("font-size", "1rem");
+            $('.bold-setting').addClass('text-muted small').removeClass('text-primary');
         }
     }
 
