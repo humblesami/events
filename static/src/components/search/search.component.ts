f27b9a780bc3cb4e809a24af29abc5ff20c956c0
NomanHameed-DN <noman.hameed@digitalnet.com>
2019-09-26 16:47:58 +0500

    align code

--- static/src/components/search/search.component.ts
+++ static/src/components/search/search.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
-import { HttpService } from '../../app/http.service';
-declare var $:any;
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import {
+  HttpService
+} from '../../app/http.service';
+declare var $: any;
 
 @Component({
   selector: 'app-search',
@@ -8,29 +16,29 @@ declare var $:any;
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  @Output() onsearch:EventEmitter<String> = new EventEmitter<String>();
+  @Output() onsearch: EventEmitter < String > = new EventEmitter < String > ();
   httpService: HttpService;
 
   constructor(private httpServ: HttpService) {
-    this.httpService = httpServ;
-   }
+      this.httpService = httpServ;
+  }
 
-   get_list(event, kw){
-     if(this.httpService.search_kw != $("#search_input").val() || this.httpService.search_kw==''){
-      this.httpService.search_kw = $("#search_input").val();
-      console.log("far ho gae");
-      this.httpService.search_kw = $("#search_input").val();
-      kw= $("#search_input").val();
-      this.onsearch.emit(kw);
-     }else{
-      console.log("ma ni labna");
-      return
+  get_list(event, kw) {
+      if (this.httpService.search_kw != $("#search_input").val() || this.httpService.search_kw == '') 
+      {
+          this.httpService.search_kw = $("#search_input").val();
+          console.log("far ho gae");
+          this.httpService.search_kw = $("#search_input").val();
+          kw = $("#search_input").val();
+          this.onsearch.emit(kw);
+      } else {
+          console.log("ma ni labna");
+          return
 
-     }
+      }
 
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
