719c68061c9d725cc05ce95cf70a46cefa7111ac
faizanansaridn <muhammad.faizan@digitalnet.com>
2019-09-30 15:15:43 +0500

    personal flag in folder's save method bug fixed

--- resources/models.py
+++ resources/models.py
@@ -19,7 +19,8 @@ class Folder(CustomModel):
         creating = False
         if not self.pk:
             creating = True
-            self.personal = self.parent.personal
+            if self.parent:
+                self.personal = self.parent.personal
         super(Folder, self).save(*args, **kwargs)
         if creating and current_user not in self.users.all():
             self.users.add(current_user.id)
