8bc416c290522fcdf5801a1777fcdbdc349f72f7
samiciit <sami.akram@digitalnet.com>
2019-11-15 17:32:38 +0500

    working on login entry

--- meetings/model_files/user.py
+++ meetings/model_files/user.py
@@ -5,8 +5,10 @@ import sys
 import uuid
 import base64
 import traceback
+
 from django.db import models
 from django.db.models import UniqueConstraint
+
 from mainapp import ws_methods
 from documents.file import File
 from django.core.files.base import ContentFile
@@ -136,14 +138,27 @@ def create_group(obj, group_name):
         return errorMessage
 
 
-class LoginEntry(CustomModel):
-    action = models.CharField(max_length=64, null=True)
+class LoginLocation(models.Model):
+    country = models.CharField(max_length=128, null=True)
+    city = models.CharField(max_length=128, null=True)
+    zip = models.CharField(max_length=16, null=True)
+    region = models.CharField(max_length=128, null=True)
+    longitude = models.CharField(max_length=16, null=True)
+    latitude = models.CharField(max_length=16, null=True)
+    time_zone = models.CharField(max_length=64, null=True)
     ip = models.GenericIPAddressField(null=True)
+
+
+class LoginEntry(models.Model):
+    name = models.CharField(max_length=64, null=True)
     user_id = models.IntegerField(null=True)
-    lc_name = models.CharField(max_length=32, null=True)
+    location = models.ForeignKey(LoginLocation, null=True, on_delete=models.SET_NULL)
     path_info = models.CharField(max_length=64, null=True)
-    session = models.CharField(max_length=32, null=True)
+    operating_system = models.CharField(max_length=32, null=True)
     login_time = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
 
     @classmethod
     def get_last_login_info(cls, user_id):
@@ -151,39 +166,69 @@ class LoginEntry(CustomModel):
         if entry:
             entry = entry[0]
             user = Profile.objects.get(pk=user_id)
+            location ={
+                'city': entry.location.city,
+                'country': entry.location.country,
+                'region': entry.location.region,
+                'zip': entry.location.zip,
+                'longitude': entry.location.longitude,
+                'latitude': entry.location.latitude,
+                'ip': entry.location.ip,
+            }
             entry = {
-                'name': user.fullname(),
-                'ip': entry.ip,
+                'operating_system': entry.operating_system,
+                'location': location,
                 'login_time': str(entry.login_time)
             }
             return entry
         else:
             return 'No login info'
 
-    @classmethod
-    def ip2location(cls, request, params):
-        ip = params['ip']
-        req_url = ip2location['prefix'] + ip + ip2location['postfix']
-        res = ws_methods.http_request(req_url)
-        res = json.loads(res)
-        return res
 
-    def __unicode__(self):
-        return '{0} - {1} - {2}'.format(self.action, str(self.user_id), self.ip)
-
-    def __str__(self):
-        return '{0} - {1} - {2}'.format(self.action, str(self.user_id), self.ip)
-
+def get_location_from_ip(ip):
+    req_url = ip2location['prefix'] + ip + ip2location['postfix']
+    print(req_url)
+    res = ws_methods.http_request(req_url)
+    res = json.loads(res)
+    return res
 
 @receiver(user_logged_in)
 def user_logged_in_callback(sender, request, user, **kwargs):
+    make_login_entry(request, user)
+
+def make_login_entry(request, user):
     meta = request.META
     ip = get_client_ip(request)
-    print('--------------------------------he-------------------------')
-    print('\n\n\n\n'+ ip+ '\n\n\n\n')
-    session = meta.get('SESSION')
+    operating_system = meta.get('SESSION')
+    time_zone = meta.get('TZ')
     path_info = meta.get('PATH_INFO')
-    login_entry = LoginEntry(ip=ip, user_id=user.id, session=session)
+    res = get_location_from_ip(ip)
+    location = LoginLocation.objects.filter(
+        time_zone=time_zone,
+        country=res.get('country_name'),
+        city=res.get('city'),
+        zip=res.get('zip'),
+        region=res.get('region_name'),
+        longitude=res.get('longitude'),
+        latitude=res.get('latitude')
+    ).first()
+    if not location:
+        location = LoginLocation.objects.create(
+            time_zone=time_zone,
+            country=res.get('country_name'),
+            city=res.get('city'),
+            zip=res.get('zip'),
+            region=res.get('region_name'),
+            longitude=res.get('longitude'),
+            latitude=res.get('latitude')
+        )
+
+    login_entry = LoginEntry(
+        location_id=location.id,
+        user_id=user.id,
+        name=str(user.id) + '-' + ip,
+        operating_system=operating_system,
+    )
     if path_info != '/rest/public':
         login_entry.path_info = path_info
     login_entry.save()
