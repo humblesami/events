1aaf5a67a07f08a3aaf2c6b0a17296822681ac60
samiciit <sami.akram@digitalnet.com>
2019-11-18 18:20:36 +0500

    config files

--- static/assets/config.js
+++ static/assets/config.js
@@ -1,44 +1,40 @@
 
 var chat_ser_port = 3000;
 var site_config_live = {
-	server_base_url:'http://boardsheet.com',
+	server_base_url:'https://www.boardsheet.com',
 	server_db : 'demo1',
-	site_url: 'http://boardsheet.com',
+	site_url: 'https://www.boardsheet.com',
 	live : true,
-    chat_server : 'https://chat.brainpbx.com',
-    app_name : 'MeetingPoint',
+    chat_server : 'https://www.chat.brainpbx.com',
+    app_name : 'BoardSheet',
 	show_logs : []
 };
 
 var site_config_local = {
-	server_base_url:'http://localhost:8000',
-	server_db : 'demo1',
+	server_base_url:'http://localhost:8000',	
 	live : false,
 	site_url: '',
-    chat_server : 'http://localhost:'+chat_ser_port,
-    app_name : 'MeetingPoint',
+    chat_server : 'http://localhost:'+chat_ser_port,    
 	show_logs : ['socket', 'ajax_before'] //, 'ajax_success'
 };
 
 var network_config = {
-	server_base_url:'http://172.16.21.170:8000',
-	server_db : 'demo1',
+	server_base_url:'http://172.16.21.170:8000',	
 	live : false,
 	site_url: '',
     chat_server : 'http://172.16.21.170:'+chat_ser_port,
-    app_name : 'MeetingPoint',
 	show_logs : ['socket','ajax_before','ajax_success']
 };
 
 var network_config_https = {
-	server_base_url:'https://172.16.21.170:8000',
-	server_db : 'demo1',
+	server_base_url:'https://172.16.21.170:8000',	
 	live : false,
 	site_url: '',
     chat_server : 'https://172.16.21.170:'+chat_ser_port,
-    app_name : 'MeetingPoint',
 	show_logs : ['socket','ajax_before','ajax_success']
 };
+network_config_https.app_name = network_config.app_name = site_config_local.app_name = site_config_live.app_name;
+network_config_https.db = network_config.db = site_config_local.db = site_config_live.db;
 
 var site_config = site_config_live;
 
