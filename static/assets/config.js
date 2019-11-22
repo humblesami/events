9a41aedd89ce9039615832ec707a2e8415e8020e
samiciit <sami.akram@digitalnet.com>
2019-11-22 15:23:52 +0500

    subdomain

--- static/assets/config.js
+++ static/assets/config.js
@@ -9,23 +9,17 @@ var site_config_live = {
     app_name : 'BoardSheet',
 	show_logs : []
 };
-var current_site_base_url = window.location.hostname + '';
-site_config_live.site_url = site_config_live.server_base_url = current_site_base_url;
-if(current_site_base_url == -1)
-{
-    alert(site_config_live.site_url, window.location.origin, current_site_base_url)
-}
 
 var site_config_local = {
 	server_base_url:'http://localhost:8000',
 	live : false,
 	site_url: '',
-    chat_server : 'http://localhost:'+chat_ser_port,    
+    chat_server : 'http://localhost:'+chat_ser_port,
 	show_logs : ['socket', 'ajax_before'] //, 'ajax_success'
 };
 
 var network_config = {
-	server_base_url:'http://172.16.21.170:8000',	
+	server_base_url:'http://172.16.21.170:8000',
 	live : false,
 	site_url: '',
     chat_server : 'http://172.16.21.170:'+chat_ser_port,
@@ -33,7 +27,7 @@ var network_config = {
 };
 
 var network_config_https = {
-	server_base_url:'https://172.16.21.170:8000',	
+	server_base_url:'https://172.16.21.170:8000',
 	live : false,
 	site_url: '',
     chat_server : 'https://172.16.21.170:'+chat_ser_port,
@@ -43,11 +37,10 @@ network_config_https.app_name = network_config.app_name = site_config_local.app_
 network_config_https.db = network_config.db = site_config_local.db = site_config_live.db;
 
 var site_config = site_config_live;
-
-
 var is_localhost = false;
+var current_site_base_url = window.location.hostname + '';
 if(current_site_base_url.indexOf('localhost') > -1)
-{   
+{
     site_config = site_config_local;
     is_localhost = true;
 }
@@ -63,8 +56,14 @@ else
     }
 }
 
-site_config.site_url = current_site_base_url;
-window['site_url'] = window['server_url'] = current_site_base_url;
+if(!is_localhost)
+{
+	site_config_live.site_url = current_site_base_url;
+	site_config_live.server_base_url = current_site_base_url;
+}
+
+window['site_url'] = site_config_live.site_url;
+window['server_url'] = site_config.server_base_url;
 site_config.public_urls = ['login']
 //console.log(site_config);
 site_config['app_name'] = 'meetings';
@@ -75,7 +74,3 @@ if(current_site_base_url.indexOf('localhost') > -1)
 }
 site_config.trace_request = 1;
 window['site_config'] = site_config;
-// if(site_config.site_url.startsWith('https://')){
-//     $('head meta:first').after('<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />');
-// }
-// console.log(site_config)
