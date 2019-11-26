b08635bcf85c0c61c8762a016478f75d312a9910
NomanHameed-DN <noman.hameed@digitalnet.com>
2019-11-26 15:10:01 +0500

    add config in cloud files

--- static/assets/config.js
+++ static/assets/config.js
@@ -13,7 +13,7 @@ var site_config_live = {
 var site_config_local = {
 	server_base_url:'http://localhost:8000',
 	live : false,
-	site_url: '',
+	site_url: 'http://localhost',
     chat_server : 'http://localhost:'+chat_ser_port,
 	show_logs : ['socket', 'ajax_before'] //, 'ajax_success'
 };
@@ -33,10 +33,8 @@ var network_config_https = {
     chat_server : 'https://172.16.21.170:'+chat_ser_port,
 	show_logs : ['socket','ajax_before','ajax_success']
 };
-network_config_https.app_name = network_config.app_name = site_config_local.app_name = site_config_live.app_name;
-network_config_https.db = network_config.db = site_config_local.db = site_config_live.db;
 
-var site_config = site_config_live;
+var site_config = {};
 var is_localhost = false;
 var current_site_base_url = window.location.protocol+'//' + window.location.hostname + '';
 console.log(current_site_base_url);
@@ -47,28 +45,15 @@ if(current_site_base_url.indexOf('localhost') > -1)
 }
 else
 {
-    if(current_site_base_url.startsWith('https://172'))
-    {
-        site_config = network_config_https;
-    }
-    else if(current_site_base_url.indexOf('172.16') > -1)
-    {
-        site_config = network_config;
-    }
+	site_config = site_config_live;
+	is_localhost = false;
 }
-
-if(!is_localhost)
-{
-	site_config_live.site_url = current_site_base_url;
-	site_config_live.server_base_url = current_site_base_url;
-}
-
-window['site_url'] = site_config_live.site_url;
+window['site_url'] = site_config.site_url;
 window['server_url'] = site_config.server_base_url;
 site_config.public_urls = ['login']
 //console.log(site_config);
 site_config['app_name'] = 'meetings';
-if(current_site_base_url.indexOf('localhost') > -1)
+if(is_localhost)
 {
     site_config.is_localhost = 1;
     site_config.trace_request = 1;
