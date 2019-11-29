e6818d0aa240ea58e79b71c0bf2d29c7345f2a5e
samiciit <sami.akram@digitalnet.com>
2019-11-29 13:31:49 +0500

    user-summary-modal

--- static/src/components/profilesummary/profilesummary.component.ts
+++ static/src/components/profilesummary/profilesummary.component.ts
@@ -3,35 +3,35 @@ import { HttpService } from '../../app/http.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-profilesummary',
-  templateUrl: './profilesummary.component.html',
-  styleUrls: ['./profilesummary.component.css',
-              '../profiledetails/profiledetails.css',
-              '../roster/roster.component.css'
-            ]
+    selector: 'app-profilesummary',
+    templateUrl: './profilesummary.component.html',
+    styleUrls: ['./profilesummary.component.css',
+        '../profiledetails/profiledetails.css',
+        '../roster/roster.component.css'
+    ]
 })
 export class ProfilesummaryComponent implements OnInit {
-  @Input() user_id: number;
-  profile_data;
-  server_url = window['server_url'];
-  constructor(private httpService: HttpService, public activeModal: NgbActiveModal) { }
-  ngOnInit() {
-    const obj_this = this;
-    let input_data = {
-      user_id: this.user_id
-    };
-    console.log(input_data, 8);
-    let args = {
-      app: 'meetings',
-      model: 'Profile',
-      method: 'get_profile_summary'
+    @Input() user_id: number;
+    profile_data;
+    server_url = window['server_url'];
+    constructor(private httpService: HttpService, public activeModal: NgbActiveModal) {}
+    ngOnInit() {
+        const obj_this = this;
+        let input_data = {
+            user_id: this.user_id
+        };
+        console.log(input_data, 8);
+        let args = {
+            app: 'meetings',
+            model: 'Profile',
+            method: 'get_profile_summary'
+        }
+        let final_input_data = {
+            params: input_data,
+            args: args
+        }
+        obj_this.httpService.get(final_input_data, (data) => {
+            obj_this.profile_data = data;
+        }, null);
     }
-    let final_input_data = {
-      params: input_data,
-      args: args
-    }
-    obj_this.httpService.get(final_input_data, (data)=>{
-      obj_this.profile_data = data;
-    }, null);
-  }
 }
